2e96e81d40eed278e8d725e310862f829f847e15
mel1bt <mberrocal.tipte@gmail.com>
2016-10-31 14:19:42 -0500

    actualizando

--- js/app.js
+++ js/app.js
@@ -1,6 +1,19 @@
 // PARA LA TRANSCION DEL FONDO 
 $(function() {
 
+    $('a[href*="#"]:not([href="#"])').click(function() {
+  if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+    var target = $(this.hash);
+    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+    if (target.length) {
+      $('html, body').animate({
+        scrollTop: target.offset().top
+      }, 1000);
+      return false;
+    }
+  }
+});
+
  $('#acerca').vegas({
         slides: [
             { src: 'img/fondo/fondo1.jpg' },
@@ -67,6 +80,4 @@ $('.wodryRX').wodry({
          });
          return false;
     }); 
-
- 
 });
